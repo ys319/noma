4ec4f9d108bd77057eec1e3b24580a6c65a1e480
Yoshimasa Kuroyama <66538624+ys319@users.noreply.github.com>
2025-08-04 11:36:37 +0900

    wip: json formatter

--- normalizeMarkdown.ts
+++ normalizeMarkdown.ts
@@ -5,53 +5,65 @@ import { unified, type Plugin } from "npm:unified"
 import { visit } from "npm:unist-util-visit"
 
 const stringifyOptions = {
-  bullet: "-",
-  listItemIndent: "one",
-  tightDefinitions: true,
-  rule: "-",
+    bullet: "-",
+    listItemIndent: "one",
+    tightDefinitions: true,
+    rule: "-",
 } satisfies RemarkStringifyOptions
 
 const baseProcessor = unified()
-  .use(remarkParse)
-  .use(remarkStringify, stringifyOptions)
+    .use(remarkParse)
+    .use(remarkStringify, stringifyOptions)
 
 const remarkRecursiveFormat: Plugin<[], Root> = () => {
-  return (tree: Root) => {
-    visit(tree, "code", (node: Code) => {
+    return (tree: Root) => {
+        visit(tree, "code", (node: Code) => {
 
-      // Markdown only.
-      const isMarkdown = node.lang === "markdown" || node.lang === "md"
-      if (!isMarkdown) {
-        return
-      }
+            // 言語タグを小文字に正規化して扱う
+            const lang = node.lang?.toLowerCase()
 
-      try {
-        // `baseProcessor`を使い、同期的にネストされたMarkdownをフォーマットする
-        const file = baseProcessor.processSync(node.value)
-        // 末尾の不要な改行を削除して上書きする
-        node.value = file.toString().trim()
-      } catch (e) {
-        // ネストされたMarkdownのパースに失敗しても、全体の処理は続行させる
-        const message = e instanceof Error ? e.message : "Unknown error"
-        console.error(`Failed to process nested markdown: ${message}`)
-      }
-    })
-  }
+            // Markdown.
+            if (lang === "markdown" || lang === "md") {
+                try {
+                    // `baseProcessor`を使い、同期的にネストされたMarkdownをフォーマットする
+                    const file = baseProcessor.processSync(node.value)
+                    // 末尾の不要な改行を削除して上書きする
+                    node.value = file.toString().trim()
+                } catch (e) {
+                    // ネストされたMarkdownのパースに失敗しても、全体の処理は続行させる
+                    const message = e instanceof Error ? e.message : "Unknown error"
+                    console.error(`Failed to process nested markdown: ${message}`)
+                }
+            }
+
+            // // JSON
+            // if (lang === 'json') {
+            //     try {
+            //         const jsonObject = JSON.parse(node.value)
+            //         node.value = JSON.stringify(jsonObject, undefined, 2)
+            //     } catch (e) {
+            //         const message = e instanceof Error ? e.message : 'Unknown error'
+            //         console.error(`Failed to format JSON: ${message}`)
+            //     }
+            //     return
+            // }
+        })
+    }
 }
 
 const processor = unified()
-  .use(remarkParse)
-  .use(remarkRecursiveFormat)
-  .use(remarkStringify, stringifyOptions)
+    .use(remarkParse)
+    .use(remarkRecursiveFormat)
+    .use(remarkStringify, stringifyOptions)
 
 export const normalizeMarkdown = async (
-  markdownText: string,
+    markdownText: string,
 ): Promise<string> => {
 
-  if (markdownText === "") {
-    return ""
-  }
+    if (markdownText === "") {
+        return ""
+    }
 
-  const file = await processor.process(markdownText)
-  return file.toString()
+    const file = await processor.process(markdownText)
+    return file.toString()
 }
